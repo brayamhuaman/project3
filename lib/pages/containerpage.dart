7322d5263c7b86d3da38b394643911b98017ad4c
Brayam <bhuamanpc@gmail.com>
2024-05-20 00:38:15 -0500

    agregar container fondo

--- lib/pages/containerpage.dart
+++ lib/pages/containerpage.dart
@@ -5,13 +5,18 @@ class ContainerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
-      style: TextStyle(),
+      style: TextStyle(fontSize: 30.0),
       child: Center(
         child: Container(
           width: 200.0,
           height: 200.0,
           color: Colors.blue,
-          child: Text("Hola flutter"),
+          padding: EdgeInsets.only(top: 20.0,right: 20.0),
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.green,
+            child: Text("Hola flutter")),
         ),
       ),
     );
